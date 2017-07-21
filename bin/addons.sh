48f0bdb3be43842331874e00d419229a68e2ee04
Eugene Sokolov <esguardian@outlook.com>
2017-07-21 13:27:01 +0300

    Исправлена опечатка

--- bin/addons.sh
+++ bin/addons.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 homedir="/opt/littlebeat"
 dialog --title "LITTLEBEAT" --backtitle "Дополнения" --infobox "Идем за дополнениями на github" 7 70
-wget https://raw.githubusercontent.com/ESGuardian/LittleBeat/master/addons/littlebeat_5.5.001_addons.sh
-rm $homedir/addons/littlebeat_5.5.001_addons.sh>/dev/nul 2>&1
-cp littlebeat_5.5.001_addons.sh $homedir/addons/littlebeat_5.5.001_addons.sh>/dev/nul 2>&1
+wget https://raw.githubusercontent.com/ESGuardian/LittleBeat/master/addons/littlebeat_5.5.001_addons.sh >/dev/nul 2>&1
+rm $homedir/addons/littlebeat_5.5.001_addons.sh >/dev/nul 2>&1
+cp littlebeat_5.5.001_addons.sh $homedir/addons/littlebeat_5.5.001_addons.sh >/dev/nul 2>&1
 rm littlebeat_5.5.001_addons.sh >/dev/nul 2>&1
 chmod +x $homedir/addons/littlebeat_5.5.001_addons.sh >/dev/nul 2>&1
-($homedir/addons/littlebeat_5.5.001_addons.sh.sh)
+bash $homedir/addons/littlebeat_5.5.001_addons.sh
 
