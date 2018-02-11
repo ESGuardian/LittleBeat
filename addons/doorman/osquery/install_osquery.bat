08c928a731ec26b3616499863b4631f526e7fdb9
Соколов Евгений <e.sokolov@angaratech.ru>
2018-02-12 00:31:43 +0300

    Переделываем. Нафиг этот дурман.

--- addons/doorman/osquery/install_osquery.bat
+++ addons/doorman/osquery/install_osquery.bat
@@ -1,8 +1,8 @@
 @echo off
 cd \ProgramData\osquery
-PowerShell.exe Invoke-WebRequest -Uri "http://littlebeat/doorman/logstash.crt" -OutFile "\ProgramData\osquery\logstash.crt" 
+rem PowerShell.exe Invoke-WebRequest -Uri "http://littlebeat/doorman/logstash.crt" -OutFile "\ProgramData\osquery\logstash.crt" 
 PowerShell.exe -ExecutionPolicy Bypass -File .\set_acl.ps1
-rem wevtutil im C:\ProgramData\osquery\osquery.man
-sc.exe create osqueryd type=own start=auto error=normal binpath="C:\ProgramData\osquery\osqueryd\osqueryd.exe --flagfile=\ProgramData\osquery\osquery.flags" displayname=osqueryd
+wevtutil im C:\ProgramData\osquery\osquery.man
+sc.exe create osqueryd type=own start=auto error=normal binpath="C:\ProgramData\osquery\osqueryd\osqueryd.exe --flagfile=\ProgramData\osquery\osquery_no_tls.flags" displayname=osqueryd
 ping 127.0.0.1 -n 6 > nul
 sc.exe start osqueryd
